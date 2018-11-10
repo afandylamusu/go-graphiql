aa5c913da4838244ac8210b9241980a9842f3444
afandylamusu <afandy.lamusu@live.co.uk>
2018-11-11 01:56:21 +0700

    update graphiql js to 0.12.0

--- server.go
+++ server.go
@@ -8,12 +8,12 @@ var Content = []byte(`
 <head>
   <style>body {height: 100vh; margin: 0; width: 100%; overflow: hidden;}</style>
   <link rel="stylesheet" href="//cdn.jsdelivr.net/sweetalert/1.1.3/sweetalert.css" />
-  <link rel="stylesheet" href="//cdn.jsdelivr.net/graphiql/0.6.3/graphiql.css" />
+  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/graphiql/0.12.0/graphiql.css" />
   <script src="//cdn.jsdelivr.net/sweetalert/1.1.3/sweetalert.min.js"></script>
   <script src="//cdn.jsdelivr.net/fetch/0.9.0/fetch.min.js"></script>
   <script src="//cdn.jsdelivr.net/react/0.14.7/react.min.js"></script>
   <script src="//cdn.jsdelivr.net/react/0.14.7/react-dom.min.js"></script>
-  <script src="//cdn.jsdelivr.net/graphiql/0.6.3/graphiql.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/graphiql/0.12.0/graphiql.js"></script>
   <script>
     (function () {
       var PROMPT_OPTIONS = {
